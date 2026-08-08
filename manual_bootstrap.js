61e3cbf7ef8562803ebf42565391afe9aad7e190
NGUYỄN NHẬT HỒNG PHƯỚC <phuocnguyen010204@gmail.com>
2026-08-08 19:35:24 +0700

    bust dedicated image script cache

--- manual_bootstrap.js
+++ manual_bootstrap.js
@@ -9,7 +9,7 @@
     'manual_solutions/de04_ch5_a.js','manual_solutions/de04_ch5_b.js','manual_solutions/de04_ch6_a.js','manual_solutions/de04_ch6_b.js',
     'manual_solutions/de04_ch7_a.js','manual_solutions/de04_ch7_b.js','manual_solutions/de04_ch8_a.js','manual_solutions/de04_ch8_b.js',
     'manual_solutions/de07.js','manual_solutions/de08.js',
-    'data_corrections.js','manual_to_static.js','app.js','image-crops.js','ux.js',
+    'data_corrections.js','manual_to_static.js','app.js','image-crops.js?v=20260808-dedicated-1','ux.js',
     'answer_audit_notes.js','beginner_theory.js','tthcm_layout.js','beginner_full_renderer.js'
   ];
 
